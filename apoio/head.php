0f9b227a44e605caa10041ba66eccff911151e68
wanessatorres <wanessa_torres@yahoo.com.br>
2016-07-20 22:49:54 -0300

    Segundo

---
+++ apoio/head.php
@@ -0,0 +1,22 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Teste Estadão Wanessa Torres</title>
+    
+    <!-- Responsive Meta Tag -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- main stylesheet -->
+    <link rel="stylesheet" href="<?php echo $siteUrl;?>css/style.css">
+    <!-- responsive stylesheet -->
+    <link rel="stylesheet" href="<?php echo $siteUrl;?>css/responsive.css">
+
+
+    <!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <!--[if lt IE 9]>
+        <script src="js/respond.js"></script>
+    <![endif]-->
+
+
+</head>
---
+++ apoio/header.php
@@ -0,0 +1,9 @@
+	<header id="masthead" class="ui page site-header" role="banner">
+
+         
+
+            
+
+           
+
+		</header><!-- END #masthead -->
