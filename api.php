144b7a5d05c2a5ff11f2068e054903131860c3b3
Rafael Basquens <rafael@basquens.com>
2016-08-03 22:54:58 -0300

    Removendo debbuger e erro reporting

--- api.php
+++ api.php
@@ -1,10 +1,5 @@
 <?php
 require_once('./Carro.class.php');
-require_once('./kint-master/Kint.class.php');
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 session_start();
 $carro = new Carro;
