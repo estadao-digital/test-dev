d20ddd758d837b082210814eeeff3cd86c11953f
Deivid Santos <deividnetwork@gmail.com>
2017-12-14 16:07:42 -0200

    Implement front-end SPA

--- index.php
+++ index.php
@@ -1,24 +1,140 @@
-<!doctype html>
-<html class="no-js" lang="">
+<!DOCTYPE html>
+<html>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title></title>
+    <title>CRUD of Cars</title>
     <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-    <link rel="apple-touch-icon" href="apple-touch-icon.png">
-    <!-- Place favicon.ico in the root directory -->
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <link rel="stylesheet" href="public/assets/css/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
     <link rel="stylesheet" href="public/assets/css/main.css">
-    <script src="public/assets/js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
+
 <body>
+<nav>
+    <div class="container">
+        <div class="row">
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo right">DeividSantosR</a>
+                <ul id="nav-mobile" class="left hide-on-med-and-down">
+                    <li><a href="sass.html">Sass</a></li>
+                    <li><a href="badges.html">Components</a></li>
+                    <li><a href="collapsible.html">JavaScript</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
+
+<div class="parallax-container">
+    <div class="parallax"><img src="public/assets/image/parallax1.jpg"></div>
+</div>
+<div class="section white">
+    <div class="row container">
+        <h2 class="header">Parallax</h2>
+        <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+    </div>
+</div>
+
+
+<div class="card-container container">
+    <div class="row">
+
+    </div>
+</div>
+
+<footer class="page-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            © 2014 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+        </div>
+    </div>
+</footer>
+
+<!-- Modal Structure -->
+<div id="modal1" class="modal modal-fixed-footer">
+    <div class="modal-content">
+        <form class="col s12">
+            <div class="row">
+                <div class="input-field col s12">
+                    <h4>Modal Header</h4>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s6">
+                    <input id="id" name="id" type="hidden" value="1">
+                    <input placeholder="1181" id="model" name="model" type="text" class="validate">
+                    <label for="model">Modelo</label>
+                </div>
+                <div class="input-field col s6">
+                    <input placeholder="BMW" id="brand" name="brand" type="text" class="validate">
+                    <label for="brand">Marca</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s6">
+                    <input placeholder="2011" id="year" name="year" type="text" class="validate">
+                    <label for="year">Ano</label>
+                </div>
+                <div class="input-field col s6">
+                    <input placeholder="R$ 49.00,00" id="price" name="price" type="text" class="validate">
+                    <label for="price">Preço</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s12">
+                    <input disabled value="public/assets/image/sample-1.jpeg" id="image" name="image" type="text" class="validate">
+                    <label for="image">URL da imagem</label>
+                </div>
+            </div>
+        </form>
+
+    </div>
+    <div class="modal-footer">
+        <div class="edit">
+            <a class="modal-action modal-close waves-effect waves-light btn red darken-1 delete"><i class="material-icons left">delete</i>Deletar</a>
+            <a class="modal-action modal-close waves-effect waves-light btn save"><i class="material-icons left">save</i>Salvar</a>
+        </div>
+        <div class="add hidden">
+            <a class="modal-action modal-close waves-effect waves-light btn grey darken-1 cancel"><i class="material-icons left">cancel</i>Cancelar</a>
+            <a class="modal-action modal-close waves-effect waves-light btn save"><i class="material-icons left">save</i>Salvar</a>
+        </div>
+    </div>
+</div>
+
+
+<div class="fixed-action-btn">
+    <a class="waves-effect waves-circle waves-light btn-floating btn-large red modal-trigger add" href="#modal1">
+        <i class="large material-icons">add</i>
+    </a>
+</div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="public/assets/js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+<script src="public/assets/js/vendor/modernizr-2.8.3.min.js"></script>
 <script src="public/assets/js/main.js"></script>
 </body>
 </html>
---
+++ public/assets/js/main.js
@@ -0,0 +1,208 @@
+var HomePage = function () {
+    'use strict';
+
+    this.apiURL = 'public/api/v1/';
+
+    this.init();
+};
+
+HomePage.prototype.init = function () {
+    var self = this;
+
+    $(document).ready(function () {
+        self.materializeComponents();
+        self.bindEvents();
+        self.loadCardItems();
+    });
+};
+
+HomePage.prototype.changeModal = function (modal, trigger) {
+    var modalTitle = modal.find('.modal-content h4');
+    var modalFooter = modal.find('.modal-footer');
+    var id = modal.find('input[name="id"]');
+    var model = modal.find('input[name="model"]');
+    var brand = modal.find('input[name="brand"]');
+    var year = modal.find('input[name="year"]');
+    var price = modal.find('input[name="price"]');
+    var image = modal.find('input[name="image"]');
+
+    if (trigger.hasClass('add')) {
+        modalTitle.text("Adicionar Carro");
+        modalFooter.find('.add').removeClass('hidden');
+        modalFooter.find('.edit').addClass('hidden');
+        id.val(null);
+        model.val(null);
+        brand.val(null);
+        year.val(null);
+        price.val(null);
+    }
+    else {
+        var cardData = trigger.parents('.item').data('cart');
+
+        modalTitle.text("Atualizar Carro");
+        modalFooter.find('.add').addClass('hidden');
+        modalFooter.find('.edit').removeClass('hidden');
+        id.val(cardData.id);
+        model.val(cardData.model);
+        brand.val(cardData.brand);
+        year.val(cardData.year);
+        price.val(cardData.price);
+    }
+};
+
+HomePage.prototype.bindEvents = function () {
+    var self = this;
+
+    $('.modal-fixed-footer .modal-footer .add .save').click(function () {
+        self.createCard(this, self);
+    });
+
+    $('.modal-fixed-footer .modal-footer .edit .save').click(function () {
+        self.updateCard(this, self);
+    });
+
+    $('.modal-fixed-footer .modal-footer .edit .delete').click(function () {
+        self.deleteCard(this, self);
+    });
+};
+
+HomePage.prototype.materializeComponents = function () {
+    var self = this;
+
+    $('.parallax').parallax();
+    $('.modal').modal({
+        ready: self.changeModal
+    });
+};
+
+HomePage.prototype.loadCardItems = function () {
+    var self = this;
+
+    $.ajax({
+        method: 'GET',
+        url: this.apiURL + 'car',
+        dataType: 'json',
+        complete: function (response) {
+            response.responseJSON.data.forEach(function (card) {
+                self.addCardItem(card);
+            });
+        }
+    });
+};
+
+HomePage.prototype.createCard = function (trigger, self) {
+    var form = $(trigger).parents('.modal-fixed-footer').find('form').get(0);
+
+    $.ajax({
+        method: 'POST',
+        url: self.apiURL + 'car',
+        data: {
+            model: form.model.value,
+            brand: form.brand.value,
+            year: form.year.value,
+            price: form.price.value,
+            image: form.image.value
+        },
+        complete: function (response) {
+            self.addCardItem(response.responseJSON);
+        }
+    });
+};
+
+HomePage.prototype.updateCard = function (trigger, self) {
+    var form = $(trigger).parents('.modal-fixed-footer').find('form').get(0);
+
+    $.ajax({
+        method: 'PUT',
+        url: self.apiURL + 'car/' + form.id.value,
+        data: {
+            model: form.model.value,
+            brand: form.brand.value,
+            year: form.year.value,
+            price: form.price.value,
+            image: form.image.value
+        },
+        complete: function (response) {
+            self.updateCardItem(response.responseJSON);
+        }
+    });
+};
+
+HomePage.prototype.deleteCard = function (trigger, self) {
+    var form = $(trigger).parents('.modal-fixed-footer').find('form').get(0);
+
+    $.ajax({
+        method: 'DELETE',
+        url: self.apiURL + 'car/' + form.id.value,
+        complete: function () {
+            self.deleteCartItem(form.id.value);
+        }
+    });
+};
+
+HomePage.prototype.addCardItem = function (cardData) {
+    $('.card-container .row').prepend(this.getCardItem(cardData));
+};
+
+HomePage.prototype.updateCardItem = function (cardData) {
+    var cardItem = $('.card-container .row').find('.item-' + cardData.id);
+
+    cardItem.data('cart', cardData);
+
+    cardItem.find('.card-title').text(cardData.brand + ' ' + cardData.model);
+};
+
+HomePage.prototype.deleteCartItem = function (cardDataId) {
+    var cardItem = $('.card-container .row').find('.item-' + cardDataId);
+
+    cardItem.remove();
+};
+
+HomePage.prototype.getCardItem = function (cardData) {
+    var cardItem = $('<div>', {
+        class: 'col s6 m4 item item-' + cardData.id
+    });
+
+    var card = $('<div>', {
+        class: 'card'
+    });
+
+    var cardImage = $('<div>', {
+        class: 'card-image',
+        append: $('<img>', {
+            src: 'public/assets/image/sample-1.jpeg'
+        })
+    });
+
+    var cardLink = $('<a>', {
+        class: 'btn-floating halfway-fab waves-effect waves-light red modal-trigger edit',
+        href: '#modal1',
+        append: $('<i>', {
+            class: 'material-icons',
+            text: 'edit'
+        })
+    });
+
+    var cardContent = $('<div>', {
+        class: 'card-content',
+        append: $('<div>', {
+            class: 'card-title',
+            text: cardData.brand + ' ' + cardData.model
+        })
+    });
+
+    cardImage.append(cardLink);
+
+    card.append(cardImage);
+    card.append(cardContent);
+
+    cardItem.append(card);
+
+    cardItem.data('cart', cardData);
+
+    return cardItem;
+};
+
+(function () {
+    var app = new HomePage();
+})();
