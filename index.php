69dd025a759be1761d84b23a8aa0a0e2f0d2f101
Deivid Santos <deividnetwork@gmail.com>
2017-12-14 01:09:41 -0200

    Add lumen framework to make API REST

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link rel="stylesheet" href="public/assets/css/normalize.css">
+    <link rel="stylesheet" href="public/assets/css/main.css">
+    <script src="public/assets/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+<body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="public/assets/js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+
+<script src="public/assets/js/main.js"></script>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->group(['prefix' => 'api/v1/'], function ($router) {
+    $router->get('car', function () {
+        echo 'asdsad';
+    });
+});
