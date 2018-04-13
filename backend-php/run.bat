480fcc226138efe276539e16589b40b58a012d18
Smith Junior <smith.junior@icloud.com>
2018-04-13 01:32:20 -0300

    Runner script!

---
+++ backend-php/run.bat
@@ -0,0 +1,23 @@
+echo "Uploading Application container""
+docker-compose up -d
+
+echo "Copying the configuration example file"
+docker exec -it web_container cp .env.example .env
+
+echo "Install dependencies"
+docker exec -it web_container composer install
+
+echo "Generate key"
+docker exec -it web_container php artisan key:generate
+
+echo "Make migrations"
+docker exec -it web_container php artisan migrate
+
+echo "Make seeds"
+docker exec -it web_container php artisan db:seed
+
+echo "Listing Routes"
+docker exec -it web_container php artisan route:list
+
+echo "Information of new containers"
+docker ps -a
