14855f718cfce185a2427bf7e9218ecad3af9082
RAFAEL COELHO DE BORJA SIMOES <P0641877@portoseguro.brasil>
2019-10-08 13:45:58 -0300

    added: Class Carro

--- application/api/classes/Carro.php
+++ application/api/classes/Carro.php
@@ -2,5 +2,22 @@
 
 class Carro
 {
-    
+    public $id, $marca, $modelo, $ano;
+
+    public function Get(){
+
+    }
+
+    public function Create(){
+
+    }
+
+    public function Update(){
+
+    }
+
+    public function Delete(){
+
+    }
+
 }
