dda4bd16aa43ed23f3a8efb9437a597a30355fe1
Luan Ramos <luan.developer@gmail.com>
2019-10-10 20:51:51 -0300

    métodos de edição e exclusão dos dados de um carro

--- app/Http/Controllers/CarroController.php
+++ app/Http/Controllers/CarroController.php
@@ -19,9 +19,9 @@ class CarroController extends Controller
         ], 200);
     }
 
-    public function obterCarro($idCarro) 
+    public function obterCarro($id) 
     {
-        $carro = Carro::where("id", $idCarro)->get();
+        $carro = Carro::where("id", $id)->get();
 
         return response()->json([
             'success'=>true, 
@@ -30,9 +30,15 @@ class CarroController extends Controller
         ], 200);
     }
 
-    public function deletarCarro($idCarro) 
+    public function deletarCarro($id) 
     {
-        $carro = Carro::where("id", $idCarro)->delete();
+        $carro = Carro::where("id", $id)->delete();
+
+        return response()->json([
+            'success'=>true, 
+            'message' => "carro deletado com sucesso!" , 
+            'data' => []
+        ], 200);
     }
 
     public function registrarCarro(Request $request) 
@@ -63,15 +69,27 @@ class CarroController extends Controller
 
     public function atualizarCarro(Request $request) 
     {
-        $dados = $request->all();
+        $dados =  (object) json_decode($request->getContent());
 
-        $carro = Carro::where("id", $dados->idCarro)->update([
+        $validator = Validator::make((array)$dados, Carro::rules());
+
+        if ($validator->fails()) 
+            return response()->json(['error'=>$validator->errors()], 401);
+
+        $carro = Carro::where("id", $dados->id)->update([
             "marca" => $dados->marca,
             "modelo" => $dados->modelo,
             "ano" => $dados->ano,
             "custo" => $dados->custo,
             "placa" => $dados->placa,
             "venda" => $dados->venda,
+            "cambio" => $dados->cambio
         ]);
+
+        return response()->json([
+            'success'=>true, 
+            'message' => "Carro atualizado com sucesso!" , 
+            'data' => Carro::find($dados->id)
+        ], 200);
     }
 }
