7dbe4e14d3faaea34ad4ab7fe785015c50332a5e
Luan Ramos <luan.developer@gmail.com>
2019-10-10 17:27:26 -0300

    escopo do controller da API - continuação

--- app/Http/Controllers/CarroController.php
+++ app/Http/Controllers/CarroController.php
@@ -3,26 +3,52 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Carro;
 
 class CarroController extends Controller
 {
-    public function listarCarros() {
-
+    public function listarCarros() 
+    {
+        $carros = Carro::all();
     }
 
-    public function obterCarros() {
-
+    public function obterCarro($idCarro) 
+    {
+        $carro = Carro::where("id", $idCarro)->get();
     }
 
-    public function deletarCarro() {
-
+    public function deletarCarro($idCarro) 
+    {
+        $carro = Carro::where("id", $idCarro)->delete();
     }
 
-    public function registrarCarro() {
+    public function registrarCarro(Request $request) 
+    {
+        $dados = $request->all();
 
+        $validade = Validator::validate();
+
+        $carro = Carro::create([
+            "marca" => $dados->marca,
+            "modelo" => $dados->modelo,
+            "ano" => $dados->ano,
+            "custo" => $dados->custo,
+            "placa" => $dados->placa,
+            "venda" => $dados->venda,
+        ]);
     }
 
-    public function atualizarCarro() {
+    public function atualizarCarro(Request $request) 
+    {
+        $dados = $request->all();
 
+        $carro = Carro::where("id", $dados->idCarro)->update([
+            "marca" => $dados->marca,
+            "modelo" => $dados->modelo,
+            "ano" => $dados->ano,
+            "custo" => $dados->custo,
+            "placa" => $dados->placa,
+            "venda" => $dados->venda,
+        ]);
     }
 }
