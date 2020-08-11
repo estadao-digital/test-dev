9298640ba5644486b01a2f0f8c03b7ecf1b3ccaf
Lucas de Carvalho <contato@lucasdecarvalho.com.br>
2020-08-11 11:21:29 -0300

    .env config and migrations

---
+++ database/migrations/2020_08_11_141424_create_cars_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand');
+            $table->string('model');
+            $table->string('year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
