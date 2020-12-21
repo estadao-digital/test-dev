9bd2371f5188ef97091007e8c90f6744c18bb989
sergio villa <sergiovilla@escavador.com>
2020-12-21 00:51:28 -0300

    Criei todas as rotas pedidas no test. Não trabalhei em nenhuma forma de autenticação por não ter sido pedido no exercício. Vou começar a trabalhar no front agora.

--- api.php
+++ api.php
@@ -3,14 +3,42 @@
     require_once  'api/v1/modelos/Carro.class.php';
     require_once 'api/v1/helpers/RotasHelper.php';
 
+    //devolve todos os carros
     get('/carros', function () {
         $carros = Carro::pegaCarros();
         return json_encode($carros);
     });
 
+    //Adiciona um novo carro no banco
+    post('/carros', function () {
+        $carro = new Carro();
+        $carro->Marca = $_POST['Marca'];
+        $carro->Modelo = $_POST['Modelo'];
+        $carro->Ano = $_POST['Ano'];
+        return json_encode($carro->salva());
+    });
 
+    //Pega um carro por ID
+    get('/carros/{id}', function ($id) {
+        $carro = Carro::pegaCarroPorId($id);
+        return json_encode($carro);
+    });
 
+    //Atualiza um carro por ID
+    put('/carros/{id}', function ($id) {
+        $carro = Carro::pegaCarroPorId($id);
+        parse_str(file_get_contents("php://input"),$_PUT);
+        $carro->Marca = $_PUT['Marca'];
+        $carro->Modelo = $_PUT['Modelo'];
+        $carro->Ano = $_PUT['Ano'];
+        return json_encode($carro->salva());
+    });
 
+    //Deleta o carro por ID
+    delete('/carros/{id}', function ($id) {
+        Carro::deleta($id);
+    });
+
+    //Estou tirando o '/api.php' das urls antes de chamar o dispatch do RotasHelper
     $acao = str_replace('/api.php', '',$_SERVER['REQUEST_URI']);
     dispatch($acao);
-?>
