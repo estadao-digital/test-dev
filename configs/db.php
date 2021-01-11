757789e09da2621ef8954a15ee8d8156177ea7ae
Emerson Udovic <emersonudovic@MacBook-Pro-de-Emerson.local>
2021-01-11 10:21:58 -0300

    readme

--- configs/db.php
+++ configs/db.php
@@ -1,8 +1,3 @@
 
     <?php
-    define('DB_TYPE','mysql');
-    define('DB_HOST','127.0.0.1');
-    define('DB_PORT','3306');
-    define('DB_USER','udovic');
-    define('DB_PASS','emers0n722236725083');
-    define('DB_NAME','make_magic');
+    
