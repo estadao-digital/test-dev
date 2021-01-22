6cd71c5fc1bfea72be36082e0ea5ac89bf2e585f
Juan Carielo <juancarielo@gmail.com>
2021-01-21 21:55:56 -0300

    feat: Configure api route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\CarController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('cars', CarController::class);
