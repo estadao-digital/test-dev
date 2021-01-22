238d651bf3052b71ff0c270466313569e5697238
Juan Carielo <juancarielo@gmail.com>
2021-01-21 21:55:27 -0300

    feat: Configure database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $this->call([
+            BrandSeeder::class,
+            ModelSeeder::class
+        ]);
     }
 }
