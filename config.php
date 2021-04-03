620c32d5b3fb332ad7fc8f945786db0ac47fbc2b
nunes <jonathan.nsdk@gmail.com>
2021-04-02 21:36:55 -0300

    correção em config

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
         'port' => '3306',
         'dbname' => 'base',
         'username' => 'root',
-        'password' => 'root',
+        'passwd' => 'root',
         "options" => [
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
