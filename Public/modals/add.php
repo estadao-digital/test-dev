e28c989188a1ddfab127eaaec51ca5e6c8ce6252
nunes <jonathan.nsdk@gmail.com>
2021-04-04 01:03:33 -0300

    Atualizando Modais

--- Public/modals/add.php
+++ Public/modals/add.php
@@ -1,34 +1,34 @@
-<div id="addEmployeeModal" class="modal fade" aria-hidden="true">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<form>
-					<div class="modal-header">						
-						<h4 class="modal-title">Novo Veículo</h4>
-						<button type="button" class="btn-close" data-bs-dismiss="modal" ></button>
+<div id="addModal" class="modal fade" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<form>
+				<div class="modal-header">
+					<h4 class="modal-title">Novo Veículo</h4>
+					<button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+				</div>
+				<div class="modal-body">
+					<div class="form-group">
+						<label>Placa</label>
+						<input type="text" class="form-control" required>
 					</div>
-					<div class="modal-body">					
-						<div class="form-group">
-							<label>Placa</label>
-							<input type="text" class="form-control" required>
-						</div>
-						<div class="form-group">
-							<label>Marca</label>
-							<input type="email" class="form-control" required>
-						</div>
-						<div class="form-group">
-							<label>Modelo</label>
-							<textarea class="form-control" required></textarea>
-						</div>
-						<div class="form-group">
-							<label>Ano</label>
-							<input type="number" class="form-control" required>
-						</div>					
+					<div class="form-group">
+						<label>Marca</label>
+						<input type="email" class="form-control" required>
 					</div>
-					<div class="modal-footer">
-						<input type="button" class="btn btn-default" data-bs-dismiss="modal"  value="Cancel">
-						<input type="submit" class="btn btn-success" value="Add">
+					<div class="form-group">
+						<label>Modelo</label>
+						<textarea class="form-control" required></textarea>
 					</div>
-				</form>
-			</div>
+					<div class="form-group">
+						<label>Ano</label>
+						<input type="number" class="form-control" required>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<input type="button" class="btn btn-default" data-bs-dismiss="modal" value="Cancel">
+					<input type="submit" class="btn btn-success" value="Add">
+				</div>
+			</form>
 		</div>
 	</div>
+</div>
--- Public/modals/edit.php
+++ Public/modals/edit.php
@@ -1,34 +1,30 @@
-<div id="editEmployeeModal" class="modal fade" aria-hidden="true">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<form>
-					<div class="modal-header">						
-						<h4 class="modal-title">Edit Employee</h4>
-						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-hidden="true"></button>
+<div id="editModal" class="modal fade" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<form>
+				<div class="modal-header">
+					<h4 class="modal-title">Editar Veículos</h4>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-hidden="true"></button>
+				</div>
+				<div class="modal-body">
+					<div class="form-group">
+						<label>Placa</label>
+						<input id='input-placa' name='placa' type="text" class="form-control" required>
 					</div>
-					<div class="modal-body">					
-						<div class="form-group">
-							<label>Name</label>
-							<input type="text" class="form-control" required>
-						</div>
-						<div class="form-group">
-							<label>Email</label>
-							<input type="email" class="form-control" required>
-						</div>
-						<div class="form-group">
-							<label>Address</label>
-							<textarea class="form-control" required></textarea>
-						</div>
-						<div class="form-group">
-							<label>Phone</label>
-							<input type="text" class="form-control" required>
-						</div>					
+					<div class="form-group">
+						<label>Modelo</label>
+						<input type="email" class="form-control" required>
 					</div>
-					<div class="modal-footer">
-						<input type="button" class="btn btn-default" data-bs-dismiss="modal" value="Cancel">
-						<input type="submit" class="btn btn-info" value="Save">
+					<div class="form-group">
+						<label>Ano</label>
+						<input id='input-ano' name='ano' type="text" class="form-control" required>
 					</div>
-				</form>
-			</div>
+				</div>
+				<div class="modal-footer">
+					<input type="button" class="btn btn-default" data-bs-dismiss="modal" value="Cancel">
+					<input type="submit" class="btn btn-info" value="Save">
+				</div>
+			</form>
 		</div>
 	</div>
+</div>
