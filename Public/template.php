62235084ba883aa40a373cc0b72f430001cb8136
nunes <jonathan.nsdk@gmail.com>
2021-04-05 12:59:51 -0300

    identações, views

--- Public/template.php
+++ Public/template.php
@@ -17,7 +17,6 @@
   
     <?php require './Public/components/container.php' ?>
     
-    
     <?php require './Public/modals/add.php' ?>
     
     <?php require './Public/modals/edit.php' ?>
@@ -25,6 +24,8 @@
     <?php require './Public/modals/delete.php' ?>
 
     
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     
