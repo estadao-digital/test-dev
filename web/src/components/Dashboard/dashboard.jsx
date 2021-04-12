30f7c809bc6e6fbdbdf08c35acba391944678e90
Anderson de Souza <anderson17ads@hotmail.com>
2021-04-12 09:15:42 -0300

    [FRONT] - Adjusting dashboard

--- web/src/components/Dashboard/dashboard.jsx
+++ web/src/components/Dashboard/dashboard.jsx
@@ -7,11 +7,18 @@ export default () => (
     <div>
         <ContentHeader title='Dashboard' small='teste Estadão' />
         <Content>
-            <h2>Bem vindo ao meu teste!</h2>
-            <p>Olá! Meu nome é <b>Anderson de Souza</b>, é com muito prazer que participo desse teste com vocês, fico feliz pela oportunidade desse desafio.</p>
+            <img src="https://assine.estadao.com.br/assets/assine/logo-estadao-cavalo.svg" />
+            <h2>Seja bem-vindo ao meu teste!</h2>
+            <p>
+                Olá, meu nome é <b>Anderson de Souza</b>! É com imenso prazer que participo desse teste com vocês e desde já agradeço a oportunidade que me foi concedida para realizar esse desafio.
+                Estou muito entusiasmado com a possibilidade de me juntar à empresa e de poder dar a minha contribuição.
+            </p>
 
             <h3>Qual é o desafio?</h3>
-            <p>Esse teste consiste em 2 etapas, uma em <b>PHP</b> e outra em <b>Front-End</b> (HTML5, CSS e JavaScript)</p>
+            
+            <p>
+                Esse teste foi concretizado em 2 etapas, sendo a primeira em <b>PHP</b> e segunda em <b>Front-End (HTML5, CSS e JavaScript)</b>.
+            </p>
         </Content>
     </div>
 )
